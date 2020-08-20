dda11e62c7c1469e3d5ef4d46ecbd449a007bfa4
quark_31 <quark_31@mail.ru>
2020-08-20 20:29:36 +0400

    homework 2

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
-alert("Привет Марафонец!");
+function getRow(firstRow, secondRow) {
+    let count1 = 0;
+    let count2 = 0;
 
+    for (let i = 0; i < firstRow.length; i++) {
+        if (firstRow.charAt(i) === 'a'){
+            count1++;
+        }
+    }
+
+    for (let i = 0; i < secondRow.length; i++) {
+        if (secondRow.charAt(i) === 'a'){
+            count2++;
+        }
+    }
+if (count1 > count2) {
+    alert(firstRow);
+}
+else alert (secondRow);
+
+}
+
+const firstRow = 'собака друг человека';
+const secondRow = 'Мама мыла раму';
+
+getRow(firstRow, secondRow);
